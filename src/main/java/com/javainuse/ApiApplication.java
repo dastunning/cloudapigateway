1b822e220686eb5882cb628400b9bdbe0c14768a
dastunning <nuruumovdastan.com@gmail.com>
2022-05-26 12:27:11 +0600

    Почистил код

--- src/main/java/com/javainuse/ApiApplication.java
+++ src/main/java/com/javainuse/ApiApplication.java
@@ -2,16 +2,9 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-//import org.springframework.web.reactive.DispatcherHandler;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -22,7 +15,6 @@ public class ApiApplication {
 
     @Bean
     public RouteLocator gateway(RouteLocatorBuilder rlb){
-        //System.out.println("requestURI" + request.getRequestURI());
 
         return rlb
                 .routes()
