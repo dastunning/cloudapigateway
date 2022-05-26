dd289cf25d5a6e3a1823654309af1aa7047662ca
dastunning <nuruumovdastan.com@gmail.com>
2022-05-26 12:08:15 +0600

    Last

--- src/main/java/com/javainuse/ApiApplication.java
+++ src/main/java/com/javainuse/ApiApplication.java
@@ -3,11 +3,14 @@ package com.javainuse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 //import org.springframework.web.reactive.DispatcherHandler;
 
 @SpringBootApplication
@@ -17,15 +20,22 @@ public class ApiApplication {
         SpringApplication.run(ApiApplication.class, args);
     }
 
-//    @Bean
-//    RouteLocator gateway(RouteLocatorBuilder rlb){
-//        return rlb
-//                .routes()
-//                .route(routeSpec -> routeSpec
-//                        .path("/api")
-//                        .filters( gatewayFilterSpec -> gatewayFilterSpec.setPath("/public")
-//                        )
-//                        .uri("http://localhost:4081/")
-//                ).build();
-//    }
+    @Bean
+    public RouteLocator gateway(RouteLocatorBuilder rlb){
+        //System.out.println("requestURI" + request.getRequestURI());
+
+        return rlb
+                .routes()
+                .route(routeSpec -> routeSpec
+                        .path("/api/**")
+                        //.filters(gatewayFilterSpec -> gatewayFilterSpec.setPath("/api/public/parking/list"))
+                        .uri("https://mobile-dev.smartparking.kz")
+                )
+                .route(routeSpec -> routeSpec
+                        .path("/payment/**")
+//                        .filters(gatewayFilterSpec -> gatewayFilterSpec.setPath("/payment/check"))
+                        .uri("https://api.parqour.com")
+                )
+                .build();
+    }
 }
